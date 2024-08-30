4237b885c035f18f49fad0e67b8668d736b3a90f
Zuhal <77770731+zuhalcode@users.noreply.github.com>
2024-08-30 20:33:13 +0700

    test deploy with comment

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+// import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   // app.setGlobalPrefix('api');
 
-  const config = new DocumentBuilder()
-    .setTitle('Tetespena API')
-    .setDescription('API for Tetespena Website ')
-    .setVersion('1.0')
-    .build();
+  // const config = new DocumentBuilder()
+  //   .setTitle('Tetespena API')
+  //   .setDescription('API for Tetespena Website ')
+  //   .setVersion('1.0')
+  //   .build();
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('/swagger', app, document, {
-    customCssUrl:
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
-    customJs: [
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js',
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js',
-    ],
-  });
+  // const document = SwaggerModule.createDocument(app, config);
+  // SwaggerModule.setup('/swagger', app, document, {
+  //   customCssUrl:
+  //     'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+  //   customJs: [
+  //     'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js',
+  //     'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js',
+  //   ],
+  // });
 
   await app.listen(process.env.PORT || 3001);
 }
